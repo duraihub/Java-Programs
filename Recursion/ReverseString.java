a793ed79a5882113bc2deffeca55eb35e688edb6
Durai <60003871+duraihub@users.noreply.github.com>
2020-02-12 22:01:59 +0530

    ReverseString was added

--- Recursion/ReverseString.java
+++ Recursion/ReverseString.java
@@ -1,1 +1,20 @@
+package com.durai;
 
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String s="Hi i am durai";
+		reverse(s);
+	}
+	public static void reverse(String s) {
+		
+		if(s.length()<=1 || s==null) {
+			System.out.print(s);
+		}else {
+			System.out.print(s.charAt(s.length()-1));
+			reverse(s.substring(0,s.length()-1));
+		}
+		
+	}
+
+}
